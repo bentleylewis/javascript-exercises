1d1d22b5d892694e99523c9e1f6ea4127694c3ed
bentleylewis <bbentleylewis@gmail.com>
2025-03-26 16:30:22 -0500

    for loop in remove function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    const result = [];
+
+    for (let i = 0; i < array1.length; i++) {
+        if (!args.includes(array1[i])) {
+            result.push(array1[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
