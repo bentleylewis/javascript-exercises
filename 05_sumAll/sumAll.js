c6e922372ab860d21c4b0595fce991e42dd12f2d
bentleylewis <bbentleylewis@gmail.com>
2025-03-26 23:37:12 -0500

    add if/for loops to iterate for sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(beginning, end) {
+    let sum = 0;
+    if (Number.isInteger(beginning) && Number.isInteger(end) && beginning >= 0 && end >=0) {
 
+        if (beginning <= end) {
+            for (let i = beginning; i < end + 1; i++) {
+                sum += i;
+            }
+        }
+        
+    
+        else {
+                for (let i = beginning; i > end - 1; i--) {
+                    sum += i;
+                }
+            
+        }
+
+        return sum;
+    }
+
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
