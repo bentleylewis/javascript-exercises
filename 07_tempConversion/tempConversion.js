7c5c84900f1f6008d35e27abf7464a02ee0a1c05
bentleylewis <bbentleylewis@gmail.com>
2025-03-27 09:25:35 -0500

    write conversion return for celcius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  return parseFloat(((fahrenheitInput-32) * (5/9)).toFixed(1));
+
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  return parseFloat((celsiusInput * (9/5) +32).toFixed(1));
+
+
+
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
