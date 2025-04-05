fe82226291e8b7fa91f8c1962545f6fc9177a8d8
bentleylewis <bbentleylewis@gmail.com>
2025-04-05 00:23:14 -0500

    recursive fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
+    let n;
+    if (typeof numArg !== "number") {
+        n = parseInt(numArg);
+    }
+    else {
+        n = numArg;
+    }
+
+    if (n < 0) return "OOPS";
+
+    if (n === 1) return 1;
+    if (n == 0) return 0;
+  
+    return fibonacci(n-1) + fibonacci (n-2);
 
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
